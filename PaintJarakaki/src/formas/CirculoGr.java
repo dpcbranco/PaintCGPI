f9cd028211be6a51392d9b18c0cf2a92f66d7454
DanielPereiraCastelo <DanielPereiraCastelo@screenshots.firefox.com>
2018-08-28 19:03:49 -0300

    Ativada função de alteração de borda.

--- PaintJarakaki/src/formas/CirculoGr.java
+++ PaintJarakaki/src/formas/CirculoGr.java
@@ -10,17 +10,19 @@ import math.Ponto;
 public class CirculoGr extends Circulo {
 	
 	Color cCor;
+	int iBorda;
 
-	public CirculoGr(Ponto p1, Ponto p2, Color cCor) {
+	public CirculoGr(Ponto p1, Ponto p2, Color cCor, int iBorda) {
 		super(p1, p2);
 		this.cCor = cCor;
+		this.iBorda = iBorda;
 	}
 	
 	public void desenharCirculo(GraphicsContext g) {
 		ArrayList<int[]> alCoordenadas = this.calcularCirculo();
 		
 		for (int[] iCoordenadas : alCoordenadas) {
-			new PontoGr(iCoordenadas[0], iCoordenadas[1], cCor).desenharPonto(g);;
+			new PontoGr(iCoordenadas[0], iCoordenadas[1], cCor, iBorda).desenharPonto(g);;
 		}
 	}
 	
--- PaintJarakaki/src/formas/LinhaGr.java
+++ PaintJarakaki/src/formas/LinhaGr.java
@@ -10,17 +10,19 @@ import math.Ponto;
 public class LinhaGr extends Linha {
 
 	Color cCor;
+	int iBorda;
 	
-	public LinhaGr(Ponto p1, Ponto p2, Color cCor) {
+	public LinhaGr(Ponto p1, Ponto p2, Color cCor, int iBorda) {
 		super(p1, p2);
 		this.cCor = cCor;
+		this.iBorda = iBorda;
 	}
 	
 	public void desenharLinha(GraphicsContext g) {
 		ArrayList<int[]> alCoordenadas = this.calcularLinha();
 		
 		for (int[] iCoordenadas : alCoordenadas) {
-			new PontoGr(iCoordenadas[0], iCoordenadas[1], cCor).desenharPonto(g);;
+			new PontoGr(iCoordenadas[0], iCoordenadas[1], cCor, iBorda).desenharPonto(g);;
 		}
 	}
 	
