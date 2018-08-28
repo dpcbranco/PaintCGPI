b91f732f9df76482943b07f86080609a6fde814c
DanielPereiraCastelo <DanielPereiraCastelo@screenshots.firefox.com>
2018-08-28 18:18:31 -0300

    Classe Janela removida. Application sendo extendido da classe Paint.

--- PaintJarakaki/src/app/Paint.java
+++ PaintJarakaki/src/app/Paint.java
@@ -1,10 +1,30 @@
 package app;
 
-import grafico.Janela;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Paint {
+public class Paint extends Application{
+	
+	Parent quadro;
+	Scene cena;
+
+	
 	public static void main(String[] args) {
-		Application.launch(Janela.class);
+		launch();
 	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		quadro = FXMLLoader.load(getClass().getResource("pj_canvas.fxml"));
+		cena = new Scene(quadro);
+		
+		primaryStage.setTitle("Paint");
+		primaryStage.setScene(cena);
+		primaryStage.show();
+	}
+
 }
